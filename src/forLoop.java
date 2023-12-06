19d406ead7ca1785855db299ae10dfd2b68b2238
rahul pawar <rahulpawar4322@gmail.com>
2023-12-06 14:14:50 +0530

    added commment

--- src/forLoop.java
+++ src/forLoop.java
@@ -3,6 +3,8 @@ public class forLoop {
 
 	public static void main(String[] args) {
 		
+		System.out.println("Hello I am rahul");
+		
 		for (int i = 0; i < 10; i++) {
 			System.out.println(i);
 		}
